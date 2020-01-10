c7e4daf9d41e0455e481d4c4a239c958bdb60a60
Ester Chang <esterchang9110@gmail.com>
2020-01-10 18:23:09 -0500

    added buttons to settings page

--- DigitalDoctor/app/src/main/java/com/juniordesign/digitaldoctor/SettingsActivity.java
+++ DigitalDoctor/app/src/main/java/com/juniordesign/digitaldoctor/SettingsActivity.java
@@ -1,9 +1,11 @@
 package com.juniordesign.digitaldoctor;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -17,5 +19,10 @@ public class SettingsActivity extends AppCompatActivity {
         mBottomNav.getMenu().findItem(R.id.navigation_home).setChecked(true);
 
         SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+    }
+
+    public void onBackButtonPressed(View view) {
+        Intent home = new Intent(this, HomeActivity.class);
+        startActivity(home);
     }
 }
