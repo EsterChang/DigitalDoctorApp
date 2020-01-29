17797ccf71e6055bca74c0108de081921f5d3e57
Cozzone, Justin Michael <jcozzone3@gatech.edu>
2020-01-29 12:29:01 -0500

    Commented Code and fixed Android Bug
    Should be easy enough to follow, let me know if any questions remain

--- DigitalDoctor/app/src/main/java/com/juniordesign/digitaldoctor/HomeActivity.java
+++ DigitalDoctor/app/src/main/java/com/juniordesign/digitaldoctor/HomeActivity.java
@@ -23,6 +23,7 @@ public class HomeActivity extends AppCompatActivity {
     private static final String SELECTED_ITEM = "arg_selected_item";
 
     private BottomNavigationView mBottomNav;
+    // keeps the index of the currently selected item.
     private int mSelectedItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +40,21 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        // physically changes the selected item on app load to the home screen
         MenuItem selectedItem;
         if (savedInstanceState != null) {
+            // Justin -- I do not believe we use this method since I do not think we keep track of states in a meaningful way
+            // Note that if we use states later, we will need to change mBottomNav.getMenu().findItem(R.id.navigation_home).setChecked(true) below;
             mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
             selectedItem = mBottomNav.getMenu().findItem(mSelectedItem);
         } else {
+            // Justin -- sets the selected icon to the home screen on application load. Otherwise, it would default to book.
             selectedItem = mBottomNav.getMenu().getItem(1);
         }
+        // Justin -- runs the select fragment method on either the saved state or the home screen
         selectFragment(selectedItem);
 
+        // since we only ever run the select fragment on the home screen currently, always set it checked on application load
         mBottomNav.getMenu().findItem(R.id.navigation_home).setChecked(true);
 
         SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
@@ -118,6 +125,7 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
+    // Justin -- saved instance state saves the currently selected icon. We do not currently use this
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putInt(SELECTED_ITEM, mSelectedItem);
@@ -125,16 +133,20 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     @Override
+    // Justin -- if android back button is pressed in the application, send back to home screen fragment
+    // if on the home screen, do the default for the android system
     public void onBackPressed() {
         MenuItem homeItem = mBottomNav.getMenu().getItem(1);
         if (mSelectedItem != homeItem.getItemId()) {
             // select home item
             selectFragment(homeItem);
+            mBottomNav.getMenu().findItem(R.id.navigation_home).setChecked(true);
         } else {
             super.onBackPressed();
         }
     }
 
+    // Justin -- find the item id on the menu and assigns the frag variable to a new instance of the correct fragment
     private void selectFragment(MenuItem item) {
         Fragment frag = null;
 
@@ -160,6 +172,7 @@ public class HomeActivity extends AppCompatActivity {
             menuItem.setChecked(menuItem.getItemId() == item.getItemId());
         }
 
+        // Justin -- if the fragment is not null, then add the fragment to the fragment container xml block
         if (frag != null) {
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.replace(R.id.fragment_container, frag);
@@ -201,6 +214,7 @@ public class HomeActivity extends AppCompatActivity {
         String message;
 
         try {
+            // reads the raw resource file and converts it to a string
             Resources res = getResources();
             InputStream in_s = res.openRawResource(R.raw.liability);
 
